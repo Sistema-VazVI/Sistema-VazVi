ad1a926d5a8d1eced790ad4cbe49947ae1bdfade
Oscar-FV <a280210@alumnos.uaslp.mx>
2023-02-24 09:43:11 -0600

    started to work in frontend

--- frontend/sistema-vazvi/src/App.tsx
+++ frontend/sistema-vazvi/src/App.tsx
@@ -1,7 +1,32 @@
+import * as React from 'react';
+import api from './api/api'
 
 function App() {
+
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+
+    getData();
+
+  }, [])
+
+  const getData = () => { 
+
+    api.get('product').then((response: any) => {
+
+      console.log(response.data);
+      setProducts(response.data);
+      console.log(products);
+
+    }).catch(error => {
+      console.log(error)
+    });
+
+  }
+
   return (
-    <h1>Hola Mundo</h1>
+    <h1>ayudaaaaaaaaaaa</h1>
   );
 }
 
