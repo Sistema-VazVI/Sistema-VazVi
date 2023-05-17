ec43b907058e009dbe6ed4a7085e9ba9f7a27604
Ricardo Barba Hernandez <rickb_hdz@outlook.com>
2023-05-16 23:49:58 -0600

    borradas boards

--- frontend/sistema-vazvi/src/_codux/boards/line/line.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Line } from '../../../components/category/category';
-
-export default createBoard({
-    name: 'Category',
-    Board: () => <Line />
-});
--- frontend/sistema-vazvi/src/_codux/boards/lineacat/lineacat.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Lineacat } from '../../../components/line/line';
-
-export default createBoard({
-    name: 'Line',
-    Board: () => <Lineacat />
-});
