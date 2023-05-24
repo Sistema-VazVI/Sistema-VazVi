46f41e467190c51b9b0c5818ce75c8e0ef0a103f
Ricardo Barba Hernandez <rickb_hdz@outlook.com>
2023-05-23 21:02:06 -0600

    revert

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -4,15 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-<<<<<<< HEAD
     origin: 'http://localhost:3000/',
   });
-=======
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-});
->>>>>>> parent of 2c714a6 (prettier format)
   await app.listen(AppModule.port);
 }
 bootstrap();
