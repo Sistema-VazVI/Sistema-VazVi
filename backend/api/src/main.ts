9db97da9f27a6066731e1a6a7c1d19447a24789d
Ricardo Barba Hernandez <rickb_hdz@outlook.com>
2023-05-23 19:52:01 -0600

    fix cors

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000/',
-  });
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
   await app.listen(AppModule.port);
 }
 bootstrap();
