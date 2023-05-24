48907e9d509f80383ecea1450abd1372cef83ed5
Ricardo Barba Hernandez <rickb_hdz@outlook.com>
2023-05-23 22:54:33 -0600

    Actualizacion estilos carrito de compra

--- frontend/sistema-vazvi/src/_codux/boards/shopping-cart/shopping-cart.board.tsx
+++ frontend/sistema-vazvi/src/_codux/boards/shopping-cart/shopping-cart.board.tsx
@@ -4,11 +4,11 @@ import { Dispatch, SetStateAction } from 'react';
 import IShoppingCart from '../../../models/shopping-cart.model';
 
 const setShoppingCart: Dispatch<SetStateAction<IShoppingCart[]>> = {} as Dispatch<SetStateAction<IShoppingCart[]>>;
-const shoppingCart: IShoppingCart[] = []; 
+const shoppingCart: IShoppingCart[] = [];
 
 export default createBoard({
     name: 'ShoppingCart',
-    Board: () => <ShoppingCart setShoppingCart={setShoppingCart} shoppingCart={shoppingCart}/>,
+    Board: () => <ShoppingCart setShoppingCart={setShoppingCart} shoppingCart={shoppingCart} />,
     environmentProps: {
         windowHeight: 640
     }
--- frontend/sistema-vazvi/src/components/shopping-cart/shopping-cart.tsx
+++ frontend/sistema-vazvi/src/components/shopping-cart/shopping-cart.tsx
@@ -1,52 +1,72 @@
 import "./shopping-cart.css";
 import React, { Dispatch, SetStateAction } from "react";
 import { TrashIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom"
-import IShoppingCart from '../../models/shopping-cart.model';
+import { Link } from "react-router-dom";
+import IShoppingCart from "../../models/shopping-cart.model";
 
 export interface ShoppingCartProps {
 	className?: string;
 	openModal?: () => void;
-    setShoppingCart: Dispatch<SetStateAction<IShoppingCart[]>>;
+	setShoppingCart: Dispatch<SetStateAction<IShoppingCart[]>>;
 	shoppingCart: IShoppingCart[];
 }
 
+export const ShoppingCart: React.FC<ShoppingCartProps> = ({
+	className = "",
+	openModal,
+	setShoppingCart,
+	shoppingCart,
+}) => {
+	const removeFromCart = (itemId: number) => {
+		setShoppingCart(shoppingCart.filter((item) => item.product.id !== itemId));
+	};
 
-export const ShoppingCart: React.FC<ShoppingCartProps> = ({ className = '', openModal, setShoppingCart, shoppingCart}) => {
-    
-    const removeFromCart = (itemId: number) => {
-        setShoppingCart(shoppingCart.filter((item) => item.product.id !== itemId));
-    };
-    
-    const calculateTotal = () => {
-        return shoppingCart.reduce((total, item) => total + (item.product.price * item.quantity), 0);
-    };
-   
-    return(
-        <div className={`${className} cartContainer`}>
-            <h2>Carrito de Compra</h2>
-            <hr />
-            <div className="cartScroll">
-                {shoppingCart.map((item: IShoppingCart) => (
-						<div>
-                            <div className="cartItemContainer">
-                                <h6 className="itemTitle">{item.product.name}</h6>
-                                <p className="itemText">Cantidad: {item.quantity} Total: {(item.quantity * item.product.price)?.toLocaleString("es-MX", { style: "currency", currency: "MXN" })}</p>
-                                <TrashIcon 
-                                    className="trashBtn" 
-                                    onClick={() => removeFromCart(item.product.id)}
-                                />
-                            </div>
-                            <hr />
-                        </div>
-					))}
-            </div>
-            <div className="cartFooter">
-                <p><span>Total: </span> {calculateTotal().toLocaleString("es-MX", { style: "currency", currency: "MXN" })} </p>
-                <hr />
-                <button className="btn btnPrimary" onClick={openModal}>Terminar Venta</button>
-            </div>
-        </div>
-    )
+	const calculateTotal = () => {
+		return shoppingCart.reduce((total, item) => total + item.product.price * item.quantity, 0);
+	};
+
+	return (
+		<div className={`${className} cartContainer`}>
+			<h2>Carrito de Compra</h2>
+			<hr />
+			<div className="cartScroll">
+				{shoppingCart.map((item: IShoppingCart) => (
+					<div>
+						<div className="shoppingCartItemContainer">
+							<div className="cartItemTextContainer">
+								<h6 className="cartItemTitle">{item.product.name}</h6>
+								<p className="cartItemText">Cantidad: {item.quantity}</p>
+								<p className="cartItemText">
+									{" "}
+									Total:{" "}
+									{(item.quantity * item.product.price)?.toLocaleString("es-MX", {
+										style: "currency",
+										currency: "MXN",
+									})}
+								</p>
+							</div>
+							<TrashIcon
+								className="trashBtn"
+								onClick={() => removeFromCart(item.product.id)}
+							/>
+						</div>
+						<hr />
+					</div>
+				))}
+			</div>
+			<div className="cartFooter">
+				<p>
+					<span>Total: </span>{" "}
+					{calculateTotal().toLocaleString("es-MX", { style: "currency", currency: "MXN" })}{" "}
+				</p>
+				<hr />
+				<button
+					className="btn btnPrimary"
+					onClick={openModal}
+				>
+					Terminar Venta
+				</button>
+			</div>
+		</div>
+	);
 };
-
