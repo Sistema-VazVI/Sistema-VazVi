72b3ba8041c77fcea8356c90b7dfc9446dfedfe5
Oscar Flores <Oscar.Flores2@flex.com>
2023-05-30 13:13:35 -0600

    final

--- db/inventariodb.sql
+++ db/inventariodb.sql
@@ -150,12 +150,6 @@ CREATE TRIGGER `payment_before_insert` BEFORE INSERT ON `payment` FOR EACH ROW B
 	 DECLARE ticketTotal FLOAT;
     DECLARE amountPayed FLOAT;
     DECLARE flag INT;
-   	
-    SELECT is_payed INTO flag FROM ticket WHERE id = NEW.ticketId;
-   
-    IF flag = 1 THEN
-    	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El Ticket Ya Ha Sido Pagado';
-    END IF;
 
     SELECT COALESCE(SUM(amount), 0) INTO amountPayed FROM payment WHERE ticketId = NEW.ticketId;
     SELECT total INTO ticketTotal FROM ticket WHERE id = NEW.ticketId;
